f39ebeab82496e2766bf7a18de20852e373a871a
GeorgeBall9 <b9029377@newcastle.ac.uk>
2024-06-07 14:25:32 +0100

    style: Update navigation links in Header.js to use Next.js Link component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,30 @@
 import Link from 'next/link';
+import { FaHome, FaInfoCircle, FaProjectDiagram, FaEnvelope } from 'react-icons/fa';
 import './Header.css';
-
 
 const Header = () => (
   <header className="header-container">
     <nav>
-      <Link href="/">Home</Link>
-      <Link href="/about">About</Link>
-      <a>Projects</a>
-      <Link href="/contact">Contact</Link>
+      <Link href="/" legacyBehavior>
+        <a className="nav-link">
+          <FaHome className="nav-icon" /> Home
+        </a>
+      </Link>
+      <Link href="/about" legacyBehavior>
+        <a className="nav-link">
+          <FaInfoCircle className="nav-icon" /> About
+        </a>
+      </Link>
+      <Link href="/#projects" legacyBehavior>
+        <a className="nav-link">
+          <FaProjectDiagram className="nav-icon" /> Projects
+        </a>
+      </Link>
+      <Link href="/contact" legacyBehavior>
+        <a className="nav-link">
+          <FaEnvelope className="nav-icon" /> Contact
+        </a>
+      </Link>
     </nav>
   </header>
 );
