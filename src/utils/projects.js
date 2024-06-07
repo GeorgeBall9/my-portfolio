35e190b020ecbd8a84b8ff9e15659bb3525c870b
GeorgeBall9 <b9029377@newcastle.ac.uk>
2024-06-07 15:16:43 +0100

    refactor: Update project descriptions in projects.js

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -3,7 +3,7 @@ const projects = [
     date: 2023,
     title: "Group Software Engineering Project",
     description:
-      "Restaurant discovery app that allows users to search for restaurants based on their location and preferences.",
+      "Restaurant discovery app.",
     link: "/projects/project-one",
     tech: ["React", "Firebase", "Zustand"],
   },
@@ -17,7 +17,7 @@ const projects = [
   {
     date: 2023,
     title: "Unify",
-    description: "A Spotify powered web application that allows people with compatible music tastes to connect!",
+    description: "A Spotify powered web application matching users.",
     link: "/projects/project-three",
     tech: ["React", "Spotify API", "Firebase","Zustand"],
   },
