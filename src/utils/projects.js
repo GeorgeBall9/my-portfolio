cd31b733d3c77aecc86fe3a0d370e89cb8210165
GeorgeBall9 <b9029377@newcastle.ac.uk>
2024-06-07 13:15:43 +0100

    Refactor projects.js to update project data structure and add new projects

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -1,23 +1,24 @@
-// src/utils/projects.js
 const projects = [
-    {
-      title: "Group Software Engineering Project",
-      description:
-        "Restaurant discovery app that allows users to search for restaurants based on their location and preferences. Users can also view restaurant details, reviews, and ratings. The app is built with React, Redux, and Firebase. It uses the MapBox API for location services and the Travel Advisor API for restaurant data.",
-      images: [
-        "/images/RestaurantApp/restaurant-app-detailspage.jpg",
-        "/images/RestaurantApp/restaurant-app-filter.jpg",
-        "/images/RestaurantApp/restaurant-app-home.jpg",
-        "/images/RestaurantApp/restaurant-app-map.jpg",
-      ],
-    },
-    {
-      title: "Project Two",
-      description: "Description of project two...",
-      images: ["/images/project-two.jpg"],
-    },
-    // Add more projects as needed
-  ];
-  
-  export default projects;
-  
+  {
+    date: "2023",
+    title: "Group Software Engineering Project",
+    description:
+      "Restaurant discovery app that allows users to search for restaurants based on their location and preferences.",
+    link: "/projects/project-one",
+  },
+  {
+    date: "2023",
+    title: "Project Two",
+    description: "Description of project two...",
+    link: "/projects/project-two",
+  },
+  {
+    date: '2023',
+    title: "Project Three",
+    description: "Description of project three...",
+    link: "/projects/project-three",
+  },
+  // Add more projects as needed
+];
+
+export default projects;
