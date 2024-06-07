af878829dab3dc8d046d25de90e8ad7341a37d4f
GeorgeBall9 <b9029377@newcastle.ac.uk>
2024-06-07 13:15:21 +0100

    feat: Add Introduction component with CSS styling

---
+++ src/components/Introduction.js
@@ -0,0 +1,12 @@
+import './Introduction.css';
+
+const Introduction = () => (
+  <section className="introduction">
+    <div className="introduction-container">
+      <h1>Hey, I'm George</h1>
+      <p>Welcome to my personal website. I'm a software engineer who loves to create and write about technology.</p>
+    </div>
+  </section>
+);
+
+export default Introduction;
