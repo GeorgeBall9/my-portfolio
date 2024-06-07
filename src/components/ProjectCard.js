a4e4207db06f2bc463f993ce51dbc00d05365d79
GeorgeBall9 <b9029377@newcastle.ac.uk>
2024-06-07 14:03:29 +0100

    style: Update ProjectCard component CSS styling and add tech icons

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,13 +1,37 @@
-import { motion } from "framer-motion";
-import Link from "next/link";
-import "./ProjectCard.css";
+import { motion } from 'framer-motion';
+import Link from 'next/link';
+import {
+  SiReact,
+  SiFirebase,
+  SiPython,
+  SiScikitlearn,
+  SiPandas,
+  SiSpotify,
+} from 'react-icons/si';
+import './ProjectCard.css';
 
-const ProjectCard = ({ date, title, description, link }) => (
+const techIcons = {
+  React: <SiReact />,
+  Firebase: <SiFirebase />,
+  Python: <SiPython />,
+  'Scikit-learn': <SiScikitlearn />,
+  Pandas: <SiPandas />,
+  'Spotify API': <SiSpotify />,
+};
+
+const ProjectCard = ({ date, title, description, link, tech }) => (
   <Link href={link}>
     <motion.div className="project-card" whileHover={{ scale: 1.05 }}>
       <time className="project-date">{date}</time>
       <h3>{title}</h3>
       <p>{description}</p>
+      <div className="tech-icons">
+        {tech.map((techName, index) => (
+          <span key={index} className="tech-icon">
+            {techIcons[techName]}
+          </span>
+        ))}
+      </div>
     </motion.div>
   </Link>
 );
