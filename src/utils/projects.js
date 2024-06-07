5539b44fb3224906211a4b5226c9e4205e8ce84d
GeorgeBall9 <b9029377@newcastle.ac.uk>
2024-06-07 12:48:48 +0100

    feat: Add projects.js file with initial project data

---
+++ src/utils/projects.js
@@ -0,0 +1,23 @@
+// src/utils/projects.js
+const projects = [
+    {
+      title: "Group Software Engineering Project",
+      description:
+        "Restaurant discovery app that allows users to search for restaurants based on their location and preferences. Users can also view restaurant details, reviews, and ratings. The app is built with React, Redux, and Firebase. It uses the MapBox API for location services and the Travel Advisor API for restaurant data.",
+      images: [
+        "/images/RestaurantApp/restaurant-app-detailspage.jpg",
+        "/images/RestaurantApp/restaurant-app-filter.jpg",
+        "/images/RestaurantApp/restaurant-app-home.jpg",
+        "/images/RestaurantApp/restaurant-app-map.jpg",
+      ],
+    },
+    {
+      title: "Project Two",
+      description: "Description of project two...",
+      images: ["/images/project-two.jpg"],
+    },
+    // Add more projects as needed
+  ];
+  
+  export default projects;
+  
