27b462420f6aebe79f9fb4c975cc637a2fd8f546
GeorgeBall9 <b9029377@newcastle.ac.uk>
2024-06-07 15:23:40 +0100

    style: Update Introduction component to include profile picture

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -2,9 +2,14 @@ import './Introduction.css';
 
 const Introduction = () => (
   <section className="introduction">
-    <div className="introduction-container">
-      <h1>Hey, I'm George</h1>
-      <p>Welcome to my personal website. I'm an aspiring software engineer who loves to create and learn about technology.</p>
+    <div className="intro-content">
+      <div className="introduction-container">
+        <h1>Hey, I'm George</h1>
+        <p>Welcome to my personal website. I'm an aspiring software engineer who loves to create and learn about technology.</p>
+      </div>
+      <div className="profile-pic-container">
+        <img src="images/profile.jpg" alt="George's Profile Picture" className="profile-pic" />
+      </div>
     </div>
   </section>
 );
